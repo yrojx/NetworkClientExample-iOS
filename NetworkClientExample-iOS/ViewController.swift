64a8406e25542bdaa79931495bb4e65b808783a5
Yossan Rahmadi <yossansr06@gmail.com>
2024-08-21 02:40:50 +0700

    Initial Commit

---
+++ NetworkClientExample-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkClientExample-iOS
+//
+//  Created by Yossan Rahmadi on 21/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NetworkClientExample-iOSUITests/NetworkClientExample_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkClientExample_iOSUITestsLaunchTests.swift
+//  NetworkClientExample-iOSUITests
+//
+//  Created by Yossan Rahmadi on 21/08/24.
+//
+
+import XCTest
+
+final class NetworkClientExample_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
